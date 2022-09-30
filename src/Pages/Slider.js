7eeb36f15eeb3a8c04ceeb9d3a7cba579826a9e1
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-09-30 23:01:56 +0600

    add more design

--- src/Pages/Slider.js
+++ src/Pages/Slider.js
@@ -2,7 +2,6 @@ import React from 'react';
 import img1 from  '../accets/img1.jpg'
 import img2 from  '../accets/img2.jpg'
 import img3 from  '../accets/img3.jpg'
-import img4 from  '../accets/img4.jpg'
 
 const Slider = () => {
     const fakeData=[
@@ -20,22 +19,30 @@ const Slider = () => {
             id : 344515,
             image : img3,
             name:"Third Image"
-        },
-        {
-            id : 324455,
-            image : img4,
-            name:"Fourth Image"
         }
     ]
     return (
-        <div className='text-5xl font-bold text-red-700 mt-20'>
-            <h2>this is silder page</h2>
-            {
-                fakeData.map(d=><>
+        <div className=' px-24 py-5  mt-20'>
+      
+         <div>
+            <h1 className='text-5xl  py-5'>Runner's workout</h1>
+            <div className='grid grid-cols-3  '>
+                <div className='relative '>
+                <img className='h-[450px] rounded-l-xl' src={img1} alt="" />
                 
-                <img src={d.image} alt="" />
-                </>)
-            }
+                <div className='absolute top-0 justify-between items-center left-0'>
+                <div className='flex justify-between  items-center px-5'>
+                    <h2>STRENGTH</h2>
+                    <span className='text-2xl font-bold'>  > </span>
+                </div>
+                </div>
+               
+                </div>
+           
+            <img className='h-[450px]' src={img2} alt="" />
+            <img className='h-[450px] rounded-r-xl ' src={img3} alt="" />
+            </div>
+         </div>
         </div>
     );
 };
