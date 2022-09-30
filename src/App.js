0f3255aad034dff3b5b465f171eae8bb98c08e98
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-09-30 22:37:04 +0600

    add more code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Pages/Navbar';
+import Slider from './Pages/Slider';
 
 function App() {
   return (
     <div >
       <Navbar/>
+      <Slider/>
     </div>
   );
 }
---
+++ src/Pages/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slider = () => {
+    return (
+        <div className='text-5xl font-bold text-red-700 mt-20'>
+            <h2>this is silder page</h2>
+        </div>
+    );
+};
+
+export default Slider;
