9dcaf42558f1808e59758b11252eea2ba6139868
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-01 09:39:47 +0600

    add more code for condition

--- src/Pages/Slider.js
+++ src/Pages/Slider.js
@@ -4,29 +4,45 @@ import img2 from  '../accets/img2.jpg'
 import img3 from  '../accets/img3.jpg'
 
 const Slider = () => {
-    const [first,setFirst]=useState(true)
+    const [first,setFirst]=useState(false)
+    const [second,setSecond]=useState(false)
+    const [third,setThird]=useState(false)
     const firstMouseIn=()=>{
-setFirst(!first)
-
+        setFirst(!first)
+        setSecond(!second)
+        // setThird(!third)
     }
     const firstMouseOut=()=>{
         setFirst(!first)
+        setSecond(!second)
+        // setThird(!third)
     }
     const secondMouseIn=()=>{
-setFirst(!first)
+        setFirst(!first)
+        setSecond(!second)
+        // setThird(!third)
+        
 
     }
     const secondMouseOut=()=>{
         setFirst(!first)
+        setSecond(!second)
+        // setThird(!third)
     }
     const thirdMouseIn=()=>{
-setFirst(!first)
+        setFirst(!first)
+        setSecond(!second)
+        setThird(!third)
 
     }
     const thirdMouseOut=()=>{
         setFirst(!first)
+        setSecond(!second)
+        setThird(!third)
     }
-    console.log(first)
+    console.log('first',first)
+    console.log("2",second)
+    console.log('3',third)
     return (
         <div className=' px-24 py-5  mt-20'>
       
@@ -34,9 +50,8 @@ setFirst(!first)
             <h1 className='text-5xl  py-5'>Runner's workout</h1>
             <div className='grid grid-cols-12  '>
 
-                <div className={`relative duration-700 col-start-1 ${first ? 'col-end-6' : 'col-end-4' } `}>
+                <div className={`relative duration-700  ${first ? 'col-start-1 col-end-4' : 'col-start-1 col-end-6' } `}>
                 <img onMouseEnter={()=>firstMouseIn()} onMouseOut={()=>firstMouseOut()} className='h-[450px] w-[1000px] rounded-l-xl' src={img1} alt="" />
-                
                 <div className='absolute top-5 grid grid-cols-12 justify-end items-center left-5'>
                 {/* <div className='flex justify-between  items-center px-5'> */}
                     <h2 className='col-start-2 col-end-5'>STRENGTH</h2>
@@ -45,7 +60,7 @@ setFirst(!first)
                 </div>
                 </div>
 
-                <div className={`relative duration-700  ${first ? 'col-start-6 col-end-9' : 'col-start-4 col-end-9' } `}>
+                <div className={`relative duration-700  ${second ? 'col-start-4 col-end-9' : 'col-start-6 col-end-10' } `}>
                 <img onMouseEnter={()=>secondMouseIn()} onMouseOut={()=>secondMouseOut()} className='h-[450px]' src={img2} alt="" />
                 
                 <div className='absolute top-5 grid grid-cols-12 justify-end items-center left-5'>
@@ -56,7 +71,7 @@ setFirst(!first)
                 </div>
                 </div>
 
-                <div className={`relative duration-700 col-start-1 ${first ? 'col-start-9 col-end-12' : 'col-start-9 col-end-13' } `}>
+                <div className={`relative duration-700 col-start-1 ${third ? 'col-start-9 col-end-12' : 'col-start-10 col-end-13' } `}>
                 <img onMouseEnter={()=>thirdMouseIn()} onMouseOut={()=>thirdMouseOut()} className='h-[450px] rounded-r-xl' src={img3} alt="" />
                 
                 <div className='absolute top-5 grid grid-cols-12 justify-end items-center left-5'>
